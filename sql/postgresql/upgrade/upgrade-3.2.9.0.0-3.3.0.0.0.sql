e75a5c061fc963fc0c1c1bab2adcbee457b9145a
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2008-05-12 13:07:18 +0000

    - fixed timesheet view

--- sql/postgresql/upgrade/upgrade-3.2.9.0.0-3.3.0.0.0.sql
+++ sql/postgresql/upgrade/upgrade-3.2.9.0.0-3.3.0.0.0.sql
@@ -2,18 +2,11 @@
 
 SELECT acs_log__debug('/packages/intranet-timesheet2-tasks/sql/postgresql/upgrade/upgrade-3.2.9.0.0-3.3.0.0.0.sql','');
 
+
+drop view im_timesheet_tasks_view;
+
 create or replace view im_timesheet_tasks_view as
-select
-	t.task_id,
-	t.material_id,
-	t.uom_id,
-	t.planned_units,
-	t.billable_units,
-	t.cost_center_id,
-	t.invoice_id,
-	t.priority,
-	t.sort_order,
-	t.gantt_project_id,
+select  t.*,
         p.parent_id as project_id,
         p.project_name as task_name,
         p.project_nr as task_nr,
@@ -22,12 +15,11 @@ select
         p.project_status_id as task_status_id,
         p.start_date,
         p.end_date,
-        p.reported_hours_cache,
-        p.reported_hours_cache as reported_units_cache
+	p.reported_hours_cache,
+	p.reported_hours_cache as reported_units_cache
 from
         im_projects p,
         im_timesheet_tasks t
 where
         t.task_id = p.project_id
 ;
-
